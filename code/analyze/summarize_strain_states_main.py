bf250bbc678553f129efbeb45f20d04e1959e135
Troy Comi <troycomi@gmail.com>
2019-03-26 13:01:58 -0400

    Summarize Strain under test

--- code/analyze/summarize_strain_states_main.py
+++ code/analyze/summarize_strain_states_main.py
@@ -3,115 +3,118 @@ import sys
 import os
 import copy
 import itertools
-import gene_predictions
-import predict
+from analyze import predict
 from collections import defaultdict
 from filter_helpers import *
-sys.path.insert(0, '..')
 import global_params as gp
-sys.path.insert(0, '../misc/')
-import read_table
-import read_fasta
+from misc import read_table
+from misc import read_fasta
 
-args = predict.process_predict_args(sys.argv[1:])
+def main():
+    args = predict.process_predict_args(sys.argv[1:])
 
-d = defaultdict(lambda: defaultdict(int))
-for species_from in args['known_states'][1:]:
+    d = defaultdict(lambda: defaultdict(int))
+    for species_from in args['known_states'][1:]:
 
-    print species_from
+        print species_from
 
-    fn_filtered1i = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
-                    'blocks_' + species_from + \
-                    '_' + args['tag'] + '_filtered1intermediate.txt'
-    fn_filtered2i = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
-                    'blocks_' + species_from + \
-                    '_' + args['tag'] + '_filtered2intermediate.txt'
+        fn_filtered1i = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
+                        'blocks_' + species_from + \
+                        '_' + args['tag'] + '_filtered1intermediate.txt'
+        fn_filtered2i = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
+                        'blocks_' + species_from + \
+                        '_' + args['tag'] + '_filtered2intermediate.txt'
 
-    regions1, fields1 = read_table.read_table_rows(fn_filtered1i, '\t')
-    regions2, fields2 = read_table.read_table_rows(fn_filtered2i, '\t')
+        regions1, fields1 = read_table.read_table_rows(fn_filtered1i, '\t')
+        regions2, fields2 = read_table.read_table_rows(fn_filtered2i, '\t')
 
-    for region_id in regions1:
+        for region_id in regions1:
 
-        strain = regions1[region_id]['strain']
-        length = int(regions1[region_id]['end']) - int(regions1[region_id]['start']) + 1
-        d[strain]['num_regions_' + species_from] += 1
-        d[strain]['num_regions_total'] += 1
-        d[strain]['num_bases_' + species_from] += length
-        d[strain]['num_bases_total'] += length
-        if regions1[region_id]['reason'] == '':
-            d[strain]['num_regions_' + species_from + '_filtered1'] += 1
-            d[strain]['num_regions_total_filtered1'] += 1
-            d[strain]['num_bases_' + species_from + '_filtered1'] += length
-            d[strain]['num_bases_total_filtered1'] += length
+            strain = regions1[region_id]['strain']
+            length = int(regions1[region_id]['end']) - int(regions1[region_id]['start']) + 1
+            d[strain]['num_regions_' + species_from] += 1
+            d[strain]['num_regions_total'] += 1
+            d[strain]['num_bases_' + species_from] += length
+            d[strain]['num_bases_total'] += length
+            if regions1[region_id]['reason'] == '':
+                d[strain]['num_regions_' + species_from + '_filtered1'] += 1
+                d[strain]['num_regions_total_filtered1'] += 1
+                d[strain]['num_bases_' + species_from + '_filtered1'] += length
+                d[strain]['num_bases_total_filtered1'] += length
 
-            alt_states = regions2[region_id]['alternative_states'].split(',')
-            for species_from_alt in alt_states:
-                d[strain]['num_regions_' + species_from_alt + \
-                          '_filtered2_inclusive'] += 1
-                d[strain]['num_bases_' + species_from_alt + \
-                          '_filtered2_inclusive'] += length
-                if species_from_alt == species_from:
-                    d[strain]['num_regions_total_filtered2_inclusive'] += 1
-                    d[strain]['num_bases_total_filtered2_inclusive'] += length
-                
-            if len(alt_states) == 1:
-                d[strain]['num_regions_' + species_from + \
-                          '_filtered2'] += 1
-                d[strain]['num_regions_total_filtered2'] += 1
-                d[strain]['num_bases_' + species_from + \
-                          '_filtered2'] += length
-                d[strain]['num_bases_total_filtered2'] += length
-
-
-            else:
-                d[strain]['num_bases_' + '_or_'.join(sorted(alt_states)) + '_filtered2i'] += length
-
-            d[strain]['num_bases_' + str(len(alt_states)) + '_filtered2i'] += length
+                alt_states = regions2[region_id]['alternative_states'].split(',')
+                for species_from_alt in alt_states:
+                    d[strain]['num_regions_' + species_from_alt + \
+                              '_filtered2_inclusive'] += 1
+                    d[strain]['num_bases_' + species_from_alt + \
+                              '_filtered2_inclusive'] += length
+                    if species_from_alt == species_from:
+                        d[strain]['num_regions_total_filtered2_inclusive'] += 1
+                        d[strain]['num_bases_total_filtered2_inclusive'] += length
+                    
+                if len(alt_states) == 1:
+                    d[strain]['num_regions_' + species_from + \
+                              '_filtered2'] += 1
+                    d[strain]['num_regions_total_filtered2'] += 1
+                    d[strain]['num_bases_' + species_from + \
+                              '_filtered2'] += length
+                    d[strain]['num_bases_total_filtered2'] += length
 
 
-strain_info = [line[:-1].split('\t') for line in open('../../100_genomes_info.txt', 'r')]
-strain_origins = dict(zip([x[0].lower() for x in strain_info], \
-                          [(x[5], x[3], x[4]) for x in strain_info]))
-for strain in d.keys():
-    d[strain]['population'] = strain_origins[strain][0]
-    d[strain]['geographic_origin'] = strain_origins[strain][1]
-    d[strain]['environmental_origin'] = strain_origins[strain][2]
+                else:
+                    d[strain]['num_bases_' + '_or_'.join(sorted(alt_states)) + '_filtered2i'] += length
 
-fn = gp.analysis_out_dir_absolute + args['tag'] + '/' + 'state_counts_by_strain.txt'
-f = open(fn, 'w')
-fields = []
+                d[strain]['num_bases_' + str(len(alt_states)) + '_filtered2i'] += length
 
-fields += ['population', 'geographic_origin', 'environmental_origin']
+    with open('/home/tcomi/projects/aclark4_introgression/100_genomes_info.txt',
+              'r') as f:
+        strain_info = [line[:-1].split('\t') for line in f]
+    strain_origins = dict(zip([x[0].lower() for x in strain_info], \
+                              [(x[5], x[3], x[4]) for x in strain_info]))
+    for strain in d.keys():
+        d[strain]['population'] = strain_origins[strain][0]
+        d[strain]['geographic_origin'] = strain_origins[strain][1]
+        d[strain]['environmental_origin'] = strain_origins[strain][2]
 
-fields += ['num_regions_' + x for x in args['known_states'][1:]]
-fields += ['num_regions_total']
-fields += ['num_regions_' + x + '_filtered1' for x in args['known_states'][1:]]
-fields += ['num_regions_total_filtered1']
-fields += ['num_regions_' + x + '_filtered2' for x in args['known_states'][1:]]
-fields += ['num_regions_total_filtered2']
-fields += ['num_regions_' + x + '_filtered2_inclusive' for x in args['known_states'][1:]]
-fields += ['num_regions_total_filtered2_inclusive']
+    fn = gp.analysis_out_dir_absolute + args['tag'] + '/' + 'state_counts_by_strain.txt'
+    f = open(fn, 'w')
+    fields = []
 
-fields += ['num_bases_' + x for x in args['known_states'][1:]]
-fields += ['num_bases_total']
-fields += ['num_bases_' + x + '_filtered1' for x in args['known_states'][1:]]
-fields += ['num_bases_total_filtered1']
-fields += ['num_bases_' + x + '_filtered2' for x in args['known_states'][1:]]
-fields += ['num_bases_total_filtered2']
-fields += ['num_bases_' + x + '_filtered2_inclusive' for x in args['known_states'][1:]]
-fields += ['num_bases_total_filtered2_inclusive']
+    fields += ['population', 'geographic_origin', 'environmental_origin']
 
-r = sorted(gp.alignment_ref_order[1:])
-for n in range(2, len(r)+1):
-    x = itertools.combinations(r, n)
-    for combo in x:
-        fields += ['num_bases_' + '_or_'.join(combo) + '_filtered2i']
-    fields += ['num_bases_' + str(n) + '_filtered2i']
+    fields += ['num_regions_' + x for x in args['known_states'][1:]]
+    fields += ['num_regions_total']
+    fields += ['num_regions_' + x + '_filtered1' for x in args['known_states'][1:]]
+    fields += ['num_regions_total_filtered1']
+    fields += ['num_regions_' + x + '_filtered2' for x in args['known_states'][1:]]
+    fields += ['num_regions_total_filtered2']
+    fields += ['num_regions_' + x + '_filtered2_inclusive' for x in args['known_states'][1:]]
+    fields += ['num_regions_total_filtered2_inclusive']
 
-f.write('strain' + '\t' + '\t'.join(fields) + '\n')
+    fields += ['num_bases_' + x for x in args['known_states'][1:]]
+    fields += ['num_bases_total']
+    fields += ['num_bases_' + x + '_filtered1' for x in args['known_states'][1:]]
+    fields += ['num_bases_total_filtered1']
+    fields += ['num_bases_' + x + '_filtered2' for x in args['known_states'][1:]]
+    fields += ['num_bases_total_filtered2']
+    fields += ['num_bases_' + x + '_filtered2_inclusive' for x in args['known_states'][1:]]
+    fields += ['num_bases_total_filtered2_inclusive']
 
-for strain in sorted(d.keys()):
-    f.write(strain + '\t')
-    f.write('\t'.join([str(d[strain][x]) for x in fields]))
-    f.write('\n')
-f.close()
+    r = sorted(gp.alignment_ref_order[1:])
+    for n in range(2, len(r)+1):
+        x = itertools.combinations(r, n)
+        for combo in x:
+            fields += ['num_bases_' + '_or_'.join(combo) + '_filtered2i']
+        fields += ['num_bases_' + str(n) + '_filtered2i']
+
+    f.write('strain' + '\t' + '\t'.join(fields) + '\n')
+
+    for strain in sorted(d.keys()):
+        f.write(strain + '\t')
+        f.write('\t'.join([str(d[strain][x]) for x in fields]))
+        f.write('\n')
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/test/analyze/test_summarize_strain_states_main.py
@@ -0,0 +1,84 @@
+import analyze.summarize_strain_states_main as main
+
+
+def test_main(mocker, capsys):
+    mocker.patch(
+        'analyze.summarize_strain_states_main.predict.process_predict_args',
+        return_value={
+            'known_states': ['state1', 'state2', 'state3'],
+            'tag': 'tag'
+        })
+    mocker.patch(
+        'analyze.summarize_strain_states_main.gp.analysis_out_dir_absolute',
+        '/dir')
+    mocker.patch(
+        'analyze.summarize_strain_states_main.gp.alignment_ref_order',
+        ['state1', 'state2', 'state3'])
+
+    mocker.patch(
+        'analyze.summarize_strain_states_main.read_table.read_table_rows',
+        side_effect=[
+            ({'r1': {'strain': 's1', 'start': 10, 'end': 20,
+                     'reason': ''},
+              'r2': {'strain': 's2', 'start': 25, 'end': 40,
+                     'reason': 'test'}},
+             ['regions']),
+            ({'r1': {'alternative_states': 'state2,state3'},
+              'r2': {'not_called': ''}},
+             ['regions']),
+            ({'r1': {'strain': 's1', 'start': 35, 'end': 40,
+                     'reason': ''},
+              'r2': {'strain': 's2', 'start': 4, 'end': 8,
+                     'reason': ''}},
+             ['regions']),
+            ({'r1': {'alternative_states': 'state3,state2'},
+              'r2': {'alternative_states': 'state3'}},
+             ['regions'])
+        ])
+
+    handle1 = mocker.MagicMock()
+    handle1.__enter__.return_value.__iter__.return_value = \
+        ("s1\tnothing\tnothing\tgeo1\tenv1\tpop1\n",
+         "s2\tnothing\tnothing\tgeo2\tenv2\tpop2\n")
+    handle2 = mocker.MagicMock()
+
+    mocker.patch(
+        'analyze.summarize_strain_states_main.open',
+        side_effect=(handle1, handle2)
+    )
+
+    main.main()
+
+    captured = capsys.readouterr().out
+    assert captured == 'state2\nstate3\n'
+
+    assert handle1.called_with(mocker.ANY, 'r')
+    assert handle2.called_with('/dirtag/state_counts_by_strain.txt', 'w')
+    calls = handle2.write.call_args_list
+    assert handle2.write.call_count == 7
+    assert calls[0][0] == \
+        ('strain\tpopulation\tgeographic_origin\tenvironmental_origin\t'
+         'num_regions_state2\tnum_regions_state3\tnum_regions_total\t'
+         'num_regions_state2_filtered1\tnum_regions_state3_filtered1\t'
+         'num_regions_total_filtered1\tnum_regions_state2_filtered2\t'
+         'num_regions_state3_filtered2\tnum_regions_total_filtered2\t'
+         'num_regions_state2_filtered2_inclusive\t'
+         'num_regions_state3_filtered2_inclusive\t'
+         'num_regions_total_filtered2_inclusive\tnum_bases_state2\t'
+         'num_bases_state3\tnum_bases_total\tnum_bases_state2_filtered1\t'
+         'num_bases_state3_filtered1\tnum_bases_total_filtered1\t'
+         'num_bases_state2_filtered2\tnum_bases_state3_filtered2\t'
+         'num_bases_total_filtered2\tnum_bases_state2_filtered2_inclusive\t'
+         'num_bases_state3_filtered2_inclusive\t'
+         'num_bases_total_filtered2_inclusive\t'
+         'num_bases_state2_or_state3_filtered2i\tnum_bases_2_filtered2i\n',)
+    assert calls[1][0] == ('s1\t',)
+    assert calls[2][0] == \
+        ('pop1\tgeo1\tenv1\t1\t1\t2\t1\t1\t2\t0\t0\t0\t2\t2\t2\t11\t6\t17\t'
+         '11\t6\t17\t0\t0\t0\t17\t17\t17\t17\t17',)
+    assert calls[3][0] == ('\n',)
+    assert calls[4][0] == ('s2\t',)
+    assert calls[5][0] == \
+        ('pop2\tgeo2\tenv2\t1\t1\t2\t0\t1\t1\t0\t1\t1\t0\t1\t1\t16\t5\t21\t0\t'
+         '5\t5\t0\t5\t5\t0\t5\t5\t0\t0',)
+    assert calls[6][0] == ('\n',)
